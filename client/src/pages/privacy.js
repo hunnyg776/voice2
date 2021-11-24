88987a0c5e507a6c4c8072e590827369cd6dd2f0
hunnyg776 <84271269+hunnyg776@users.noreply.github.com>
2021-11-24 19:02:45 +0530

    Update privacy.js

--- client/src/pages/privacy.js
+++ client/src/pages/privacy.js
@@ -1,28 +1,7 @@
 
 const privacy = () => {
   return (
-    <><div class="tab">
-      <button class="tablinks" onclick="openCity(event, 'London')">London</button>
-      <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
-      <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
-    </div><div id="London" class="tabcontent">
-        <h3>London</h3>
-        <p>London is the capital city of England.</p>
-      </div><div id="Paris" class="tabcontent">
-        <h3>Paris</h3>
-        <p>Paris is the capital of France.</p>
-      </div><div id="Tokyo" class="tabcontent">
-        <h3>Tokyo</h3>
-        <p>Tokyo is the capital of Japan.</p>
-      </div><><div class="topnav" id="myTopnav">
-        <a href="#home" class="active">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-          <i class="fa fa-bars"></i>
-        </a>
-    </div>
+    
          
             <h1> Privacy Policy</h1> <br />
             <h4> Effective October 10, 2021</h4> <br />
